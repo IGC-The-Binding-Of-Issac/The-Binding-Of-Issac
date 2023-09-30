73985320f3ddd1fd0f570b1a6863a7c5f524cb48
Dayoeyo <dmschd0817@naver.com>
2023-09-30 15:17:48 +0900

    0930 1517
    Bomb and Coin

--- Assets/Item/Bomb/BombScript/DropBomb.cs
+++ Assets/Item/Bomb/BombScript/DropBomb.cs
@@ -4,24 +4,37 @@ using UnityEngine;
 
 public class DropBomb : MonoBehaviour
 {
+    private Animator gb;
+    private Rigidbody2D rb;
+    private int[] direction = { -1, 1 };
     private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        gb = GetComponent<Animator>();
+
+        int randomX = Random.Range(0, 2);
+        int randomY = Random.Range(0, 2);
+        //최초 등장 시 (왼쪽, 오른쪽 / 위 , 아래) 방향을 랜덤으로 정해 움직이기 위한 AddForce
+        rb.AddForce(new Vector2(direction[randomX], direction[randomY]), ForceMode2D.Impulse);
+
         // 최초 드랍 애니메이션 필요.
+        //gb.SetTrigger("DropBomb");
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
         {
-            GetBomb();
+            gb.SetTrigger("GetBomb");
             ItemManager.instance.bombCount++;
-            Destroy(gameObject);
         }
     }
 
 
+    //먹었을 때 애니메이션 이벤트 (사라지게 만들기)
     void GetBomb()
     {
-        // 먹었을때 폭탄 애니메이션 작성.
+        Destroy(gameObject);
     }
 }
--- Assets/Item/Coin/Coin.cs
+++ Assets/Item/Coin/Coin.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     Animator coin_animator;
+    private Rigidbody2D rb;
     public void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        float randomX = Random.Range(-1.0f, 1.0f);
+        float randomY = Random.Range(-1.0f, 1.0f);
+        float randomForce = Random.Range(50f, 70f);
+        rb.AddForce(new Vector2(randomX, randomY) * randomForce);
         coin_animator = GetComponent<Animator>();
     }
 
