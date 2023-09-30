7d6284952e610e0d87266901311847df6a5a7fe4
Dayoeyo <dmschd0817@naver.com>
2023-09-30 15:25:27 +0900

    0930 1525
    Bomb And Coin

--- Assets/Item/Bomb/BombScript/DropBomb.cs
+++ Assets/Item/Bomb/BombScript/DropBomb.cs
@@ -6,16 +6,16 @@ public class DropBomb : MonoBehaviour
 {
     private Animator gb;
     private Rigidbody2D rb;
-    private int[] direction = { -1, 1 };
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         gb = GetComponent<Animator>();
 
-        int randomX = Random.Range(0, 2);
-        int randomY = Random.Range(0, 2);
+        float randomX = Random.Range(-1.0f, 1.0f);
+        float randomY = Random.Range(-1.0f, 1.0f);
+        float randomForce = Random.Range(50f, 70f);
         //최초 등장 시 (왼쪽, 오른쪽 / 위 , 아래) 방향을 랜덤으로 정해 움직이기 위한 AddForce
-        rb.AddForce(new Vector2(direction[randomX], direction[randomY]), ForceMode2D.Impulse);
+        rb.AddForce(new Vector2(randomX, randomY) * randomForce);
 
         // 최초 드랍 애니메이션 필요.
         //gb.SetTrigger("DropBomb");
