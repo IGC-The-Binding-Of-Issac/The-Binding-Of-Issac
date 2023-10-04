91a32209630870afebf0a45dbffae6c3bf6bf8e1
Dayoeyo <dmschd0817@naver.com>
2023-10-04 18:11:05 +0900

    1004 1810
    패시브 아이템 추가

--- Assets/Item/ItemInfo.cs
+++ Assets/Item/ItemInfo.cs
@@ -6,17 +6,6 @@ using UnityEngine;
 public class ItemInfo : MonoBehaviour
 {
     public int itemCode;
-    public string itemName;
-
-    public float item_Damage; //공격력 증가 (+, -)
-    public float item_DamageMulti; //공격력 증가 (*, /)
-    public float item_Hp; //체력 증가 (+, -)
-    public float item_MaxHP; //최대 체력 증가 (+, -)
-    public float item_AttackSpeed; //공격속도 증가 (+, -)
-    public float item_Range; //사거리 증가 (+, -)
-    public float item_MoveSpeed; //이동속도 증가 (+, -)
-    public float item_ShotSpeed; //눈물속도 증가 (+, -)
-    public float item_Luck; //행운 증가 (+, -)
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -38,4 +27,9 @@ public class ItemInfo : MonoBehaviour
     }
 
     public virtual void UseItem() { Debug.Log("재정의 해줘!"); }
+
+    public void SetItemCode(int code)
+    {
+        itemCode = code;
+    }
 }
---
+++ Assets/Item/Passive/Holy Mantle [7]/HolyMantle.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class HolyMantle : ItemInfo
+{
+    private void Start()
+    {
+        SetItemCode(7);
+    }
+
+    public override void UseItem()
+    {
+        //몸 주위에 방어막이 생긴다. 피격 시 방어막 사라지고 1초간 무적.
+        //방 이동 시 방어막 재생성
+    }
+}
