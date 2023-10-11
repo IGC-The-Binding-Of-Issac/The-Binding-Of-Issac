1669a5217c34b6c722340e6214902bd422a68323
youcheae <kjhstar2486@naver.com>
2023-10-12 01:23:19 +0900

    1012
    1012 : Tride 유한상태머신으로 구현중,
    Move스크립트에 state 변환 코드 넣어줘야함
    (총알 발사하듯이 시간으로 state변환 하면 될듯?)

--- Assets/Enemy/Script/Spider/Tride.cs
+++ Assets/Enemy/Script/Spider/Tride.cs
@@ -2,26 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum TrideState 
+{
+    Idle,
+    Tracking, // 추적
+    Jump // 점프
+}
+
+
 public class Tride : Top_Spider
 {
     // 방에 들어오면 추적,
-    // 일정 시간 마다 점프 (범위 밖에 있으면 아얘 글로 멀리 점프)
-    enum TrideState 
-    {
-        Tracking, //추적
-        Jump // 점프
-            
-    }
-
+    // 일정 시간 마다 점프 (플레이어를 향해 바로)
+    [SerializeField] TrideState trideState;
 
     void Start()
     {
+        ChageState(TrideState.Idle); //초기 상태는 가만히 있는, 
         Spider_Move_InitialIze();
 
         playerInRoom = false;
         dieParameter = "isDie";
 
-        //최상위 enemy
+        //Enemy
         hp = 20f;
         sight = 3f;
         moveSpeed = 10f;
@@ -31,11 +34,54 @@ public class Tride : Top_Spider
     private void Update()
     {
         if (playerInRoom) 
-        { }
+        {
+            Move();
+        }
     }
 
     public override void Move()
     {
-       
+       // state변화 시키는 스크립트 작성
+
+    }
+
+    // 플레이어의 행동을 newState로 변환
+    void ChageState(TrideState newState) 
+    {
+        // 이전 상태 종료
+        StopCoroutine(trideState.ToString());
+        // 새로운 상태로 변경
+        trideState = newState;
+        // 현재 상태의 코루틴 실행
+        StartCoroutine(trideState.ToString());
+    }
+
+    
+    IEnumerator Idle() 
+    {
+        //상태 초기화
+        yield return null;
+    }
+    IEnumerator Tracking()
+    {
+        //상태로 진입 할 때 1회 출력
+        Debug.Log("Tride : " + trideState);
+
+        while (true) 
+        {
+            Debug.Log("Tride가 플레이어를 추적 해야함");
+            yield return null;
+        }
+    }
+    IEnumerator Jump()
+    {
+        //상태로 진입 할 때 1회 출력
+        Debug.Log("Tride : " + trideState);
+
+        while (true) 
+        {
+            Debug.Log("Tride가 점프를 해야함 ");
+            yield return null;
+        }
     }
 }
