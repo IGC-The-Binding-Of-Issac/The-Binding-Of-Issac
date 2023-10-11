f3b86917e0dd7ba549b745e635e8eccaab215231
binanuku <binanuku@gmail.com>
2023-10-11 14:16:19 +0900

    플레이어와 적 충돌 수정

--- Assets/Enemy/Script/Enemy.cs
+++ Assets/Enemy/Script/Enemy.cs
@@ -44,7 +44,7 @@ public class Enemy : MonoBehaviour
     }
 
     //collider°Ë»ç
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
