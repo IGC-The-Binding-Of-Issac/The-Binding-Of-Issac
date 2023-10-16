02cb1b4b5a9c8b661a003bfaa25834f7dcca01f1
youcheae <kjhstar2486@naver.com>
2023-10-16 19:39:28 +0900

    1016

---
+++ Assets/Enemy/Script/Issac/Pacer.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pacer : Top_IssacMonster
+{
+    bool paberisFlipped;
+    void Start()
+    {
+        Issac_Move_InitialIze();
+
+        playerInRoom = false;
+        dieParameter = "isDie";
+
+        // Enemy
+        hp = 5f;
+        sight = 5f;
+        moveSpeed = 1.5f;
+        waitforSecond = 0.5f;
+
+        // Top_IssacMonster
+        randRange = 3f;
+        fTime = 2f;
+        StartCoroutine(checkPosi(randRange));
+
+        //pacer
+        paberisFlipped = false;
+    }
+
+    void Update()
+    {
+        if (playerInRoom)
+            justTrackingPlayerPosi = GameObject.FindWithTag("Player").transform;
+            Move();
+    }
+
+    public override void Move()
+    {
+        Prwol();
+    }
+
+    //플레이어 위치에 따라 그쪽을 보는 애니메이션
+    /*
+    public void PaverTurn()
+    {
+        if (transform.position.x > justTrackingPlayerPosi.position.x && paberisFlipped)
+        {
+            animator.SetBool("isRight" , true);
+            paberisFlipped = false;
+        }
+        else if (transform.position.x < justTrackingPlayerPosi.position.x && !paberisFlipped)
+        {
+            animator.SetBool("isRight", false);
+            paberisFlipped = true;
+        }
+    }
+    */
+}
--- Assets/Enemy/Script/Spider/Spider.cs
+++ Assets/Enemy/Script/Spider/Spider.cs
@@ -1,10 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
+
+public enum SpiderState 
+{ 
+    SpiderStay,
+    SpiderRandMove
+}
 
 public class Spider : Top_Spider
 {
-    //  랜덤 움직임 + 플레이어 감지하면 플레이어한테 지그재그로 따라가기(?)
+    //  랜덤 움직임
+    [SerializeField] SpiderState state;
+    float currTime;
+    float spdierMoveTime;
 
     void Start()
     {
@@ -13,30 +24,80 @@ public class Spider : Top_Spider
         playerInRoom = false;
         dieParameter = "isDie";
 
-        //최상위 enemy
-        hp = 2f;
-        sight = 3f;
-        moveSpeed = 5f;
-        waitforSecond = 0.5f;
+        //Enemy
+        animator = GetComponent<Animator>();
+        hp = 5f;
+        sight = 4f;
+        moveSpeed = 2f;
+        waitforSecond = 1f;
+        attaackSpeed = 0.5f; // stay <-> move
 
-        //상위 Top_Spider
-        randRange = 10f;
-        fTime = 0.5f; 
+        //TopFly
+        randRange = 3f;
+        fTime = 0.5f;
         StartCoroutine(checkPosi(randRange));
-        
-    }
 
-    void Update()
+        //Spider
+        currTime = attaackSpeed;
+        spdierMoveTime = 3f;
+    }
+    private void Update()
     {
         if (playerInRoom)
         {
             Move();
         }
+
     }
 
     public override void Move()
     {
-        Prwol();
+        //상태 변화
+        currTime += Time.deltaTime;
+        if (currTime < attaackSpeed)
+        {
+            ChageState(SpiderState.SpiderStay);
+        }
+        else if (currTime < spdierMoveTime)
+        {
+
+            ChageState(SpiderState.SpiderRandMove);
+        }
+        else
+        {
+            // 타이머를 재설정하고 "SpiderStay" 상태로 돌아감
+            animator.SetBool("isMove", false);
+            currTime = 0;
+            ChageState(SpiderState.SpiderStay);
+        }
+    }
+
+    void ChageState(SpiderState newState)
+    {
+        // 이전 상태 종료
+        StopCoroutine(state.ToString());
+        // 새로운 상태로 변경
+        state = newState;
+        // 현재 상태의 코루틴 실행
+        StartCoroutine(state.ToString());
+    }
+
+    IEnumerator SpiderStay()
+    {
+        while (true)
+        {
+            //아무것도 안함
+            yield return null;
+        }
+    }
+    IEnumerator SpiderRandMove()
+    {
+        animator.SetBool("isMove", true);
+        while (true)
+        {
+            Prwol();
+            yield return null;
+        }
     }
 
 }
