b36fdbddef78e7e92cae99497508877689016b24
youcheae <kjhstar2486@naver.com>
2023-10-17 18:16:05 +0900

    1017_Enemy

--- Assets/Enemy/Script/Spider/Tride.cs
+++ Assets/Enemy/Script/Spider/Tride.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.UIElements;
 
 public enum TrideState 
 {
@@ -19,50 +20,41 @@ public class Tride : Top_Spider
     // 유한상태 머신 사용 (코루틴)
     // yield return null : 해당 상태로 넘어오면 , 다른 상태로 넘어갈 때 까지 while문 진행
 
-    [Header("Tride")]
     [SerializeField] TrideState trideState;
     float currTime;
-    float jumpCoolTIme;
+    float trideJumpTime;
     float oriSpeed;
     float jumpSpeed;
-    float trideX;
-    float trideY;
-    float trideZ;
 
     void Start()
     {
-        ChageState(TrideState.TrideIdle); //초기 상태는 가만히 있는, 
         Spider_Move_InitialIze();
 
         playerInRoom = false;
         dieParameter = "isDie";
-        animator = GetComponent<Animator>();
 
         //Enemy
-        hp = 1f;
-        sight = 3f;
+        animator = GetComponent<Animator>();
+        hp = 5f;
+        sight = 4f;
         moveSpeed = 2f;
         waitforSecond = 0.8f;
-        attaackSpeed = 2f;
+        attaackSpeed = 3f; // stay <-> move
 
-        //Tride
+        //Spider
         currTime = 0;
-        jumpCoolTIme = 1f;
+        trideJumpTime = 1.2f + attaackSpeed;
         oriSpeed = moveSpeed;
-        jumpSpeed = 7f;
+        jumpSpeed = moveSpeed * 2;
     }
-
     private void Update()
     {
-        justTrackingPlayerPosi = GameObject.FindWithTag("Player").transform;
-        if (justTrackingPlayerPosi == null)
-            return;
-
-        if (playerInRoom) 
+        if (playerInRoom)
         {
+            justTrackingPlayerPosi = GameObject.FindGameObjectWithTag("Player").transform;
             Move();
-            Lookplayer(justTrackingPlayerPosi);
         }
+
     }
 
     public override void Move()
@@ -73,25 +65,22 @@ public class Tride : Top_Spider
         {
             ChageState(TrideState.TrideTracking);
         }
-        else if (currTime <attaackSpeed + jumpCoolTIme)
+        else if (currTime < trideJumpTime)
         {
-
             ChageState(TrideState.TrideJump);
         }
         else
         {
             // 타이머를 재설정하고 "Tracking" 상태로 돌아감
             animator.SetBool("isJump", false);
+            moveSpeed = oriSpeed;
+            transform.position = new Vector3(transform.position.x, transform.position.y, 0);
             currTime = 0;
             ChageState(TrideState.TrideTracking);
         }
-
-
     }
 
-
-    // 플레이어의 행동을 newState로 변환
-    void ChageState(TrideState newState) 
+    void ChageState(TrideState newState)
     {
         // 이전 상태 종료
         StopCoroutine(trideState.ToString());
@@ -101,46 +90,24 @@ public class Tride : Top_Spider
         StartCoroutine(trideState.ToString());
     }
 
-    
-    IEnumerator Idle() 
+    //상태 
+    IEnumerator TrideTracking()
     {
-        //상태 초기화
-        yield return null;
-    }
-
-    IEnumerator Tracking()
-    {
-        while (true) 
+        while (true)
         {
-            moveSpeed = oriSpeed;
             Tracking(justTrackingPlayerPosi);
             yield return null;
         }
     }
-    IEnumerator Jump()
+    IEnumerator TrideJump()
     {
+        moveSpeed = jumpSpeed;
         animator.SetBool("isJump", true);
-        while (true) 
+        transform.position += new Vector3(0, 0, -1) * Time.deltaTime;
+        while (true)
         {
-            moveSpeed = jumpSpeed;
             Tracking(justTrackingPlayerPosi);
-            //Tride가 점프하는 코드
             yield return null;
-
         }
-    }
-
-    void TrideJump() 
-    {
-        // 목표 위치 받아옴 (플레이어 위치)
-
-        trideX = justTrackingPlayerPosi.position.x;
-        trideY = justTrackingPlayerPosi.position.y;
-        trideZ = justTrackingPlayerPosi.position.z;
-
-        // x축 , y축 플레이어 방향으로 tracking하듯이
-        // z측 : 그냥 바로 1로 만들었다가 끝나면 0으로 바꾸기
-
-
     }
 }
