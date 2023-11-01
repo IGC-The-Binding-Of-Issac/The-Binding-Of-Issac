be013f37425864c5c586d0dd0019c1cf084bb85b
youcheae <kjhstar2486@naver.com>
2023-11-01 16:37:36 +0900

    1101_larry_Movement

---
+++ Assets/Enemy/Script/Boss/HeadDetective.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadDetective : MonoBehaviour
+{
+    [SerializeField] SnakeManager parent;
+
+    void Start()
+    {
+        parent = transform.parent.GetComponent<SnakeManager>();
+    }
+
+// Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = parent.snakeBody[0].transform.position;
+    }
+
+    //충돌처리
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall")) 
+        {
+            //벽이랑 충돌하면 리셋
+            parent.stateReset();
+            Debug.Log("벽충돌");
+
+        }
+    }
+
+
+}
