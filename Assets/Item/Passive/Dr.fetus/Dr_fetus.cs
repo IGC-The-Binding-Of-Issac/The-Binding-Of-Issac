e3594a5c0da97c92e142fc03dba4d2056d16735a
Dayoeyo <dmschd0817@naver.com>
2023-11-06 19:48:58 +0900

    눈물 레이어 수정

---
+++ Assets/Item/Passive/Dr.fetus/Dr_fetus.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class Dr_fetus : ItemInfo
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetItemCode(0);
+    }
+
+    public override void UseItem()
+    {
+
+    }
+}
--- Assets/Script/Player Script/PlayerController.cs
+++ Assets/Script/Player Script/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -349,6 +350,8 @@ public void Shoot(float x, float y)
         if (Input.GetKey(KeyCode.UpArrow))
         {
             playerShotAnim.SetBool("playerUpShot", true);
+            if(tear != null)
+                tear.GetComponent<SpriteRenderer>().sortingOrder = 102;
         }
         else
         {
