7b0a9723b202a47400163efcb149831b12b99bd5
youcheae <kjhstar2486@naver.com>
2023-11-20 15:23:20 +0900

    1120_FSM_JumpComplete

--- Assets/Enemy/Script/Enemy_Jump.cs
+++ Assets/Enemy/Script/Enemy_Jump.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy_Jump : TEnemy_FSM<TEnemy>
 {
     [SerializeField] TEnemy e_Owner;                          // 주인 변수
+    float currTIme;
+    float stateTime;
 
     public Enemy_Jump(TEnemy _ower)                       // 생성자 초기화
     {
@@ -15,17 +19,22 @@ public class Enemy_Jump : TEnemy_FSM<TEnemy>
     {
         Debug.Log("jump 상태 진입");
         e_Owner.eCurState = TENEMY_STATE.Jump;              // 현재 상태를 TENEMY_STATE의 Tracking으로 
-
-        e_Owner.e_moveInialize();                           // 내 위치 1회 받아오기
-        e_Owner.e_findPlayer();                             // 플레이어 위치 1회 받아오기
         e_Owner.e_jumpSet();                                // 점프 전 세팅
 
-        e_Owner.e_jumpAni();                                // 점프 애니메이션 실행
+        stateTime = Random.Range(0.1f, 1.2f);
+        currTIme = e_Owner.getattaackSpeed + stateTime;   // 공격속도 가져오기
+
     }
 
     public override void Excute()
     {
-        e_Owner.e_Tracking();                                       // tracking
+        e_Owner.e_Tracking(e_Owner.JumpSpeed);
+
+        currTIme -= Time.deltaTime;
+        if (currTIme <= 0) 
+        {
+            e_Owner.e_doneJump();
+        }
 
         if (e_Owner.e_isDead())                                     // 몬스터가 죽으면 
         {
