a3648d45ce7788d332bd80b8c06de5caa1955652
youcheae <kjhstar2486@naver.com>
2023-11-21 18:35:54 +0900

    1121_EnemyBullet

---
+++ Assets/Enemy/Script/Bullet/EnemyFollowBullet.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : Enemy_Bullet
+{
+    /// <summary>
+    /// Player 따라가는 총알
+    /// </summary>
+    [SerializeField] Vector3 bulletDesti;
+    [SerializeField] Transform playerPosi;
+
+
+    bool isMoving = true;
+
+    private void Start()
+    {
+        ani             = GetComponent<Animator>();
+        playerPosi      = GameObject.FindWithTag("Player").transform; 
+        bulletDesti     = new Vector3(playerPosi.position.x, playerPosi.position.y, 0);
+
+        waitForDest     = 0.5f;
+        bulletSpeed     = 5f;
+    }
+
+    private void Update()
+    {
+        if (isMoving)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, bulletDesti, bulletSpeed * Time.deltaTime); 
+            bulletDestroy(); 
+        }
+    }
+    void bulletDestroy()
+    {
+        if (Vector3.Distance(gameObject.transform.position, bulletDesti) <= 0.5f)
+        {
+            ani.SetBool("bulletDestroy", true);
+            Destroy(gameObject, waitForDest);
+        }
+    }
+}
--- Assets/Enemy/Script/Bullet/Enemy_Bullet.cs
+++ Assets/Enemy/Script/Bullet/Enemy_Bullet.cs
@@ -2,56 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyBullet : MonoBehaviour
+public class Enemy_Bullet : MonoBehaviour
 {
-    Animator ani;
-    [SerializeField] Vector3 bulletDesti;
-    [SerializeField] Transform playerPosi;
-
-    float bulletSpeed;
-    float waitForDest;
-
-
-    bool isMoving = true;
-    private void Start()
-    {
-        ani = GetComponent<Animator>();
-        playerPosi = GameObject.FindWithTag("Player").transform; //플레이어의 위치
-        bulletDesti = new Vector3(playerPosi.position.x, playerPosi.position.y, 0);
-
-        waitForDest = 0.5f;
-        bulletSpeed = 5f;
-    }
-
-    private void Update()
-    {
-        if(isMoving)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, bulletDesti, bulletSpeed * Time.deltaTime); //총알 움직임
-            bulletDestroy(); // 도착하면 destory
-        }
-    }
+    protected Animator ani;
+    protected float bulletSpeed;
+    protected float waitForDest;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player")) // 플레이어랑 닿이면 삭제
         {
-            isMoving = false;
             ani.SetBool("bulletDestroy", true);
             PlayerManager.instance.GetDamage(); //플레이어랑 데미지
             Destroy(gameObject, waitForDest);
         }
         else if (collision.gameObject.CompareTag("Wall") || collision.gameObject.CompareTag("Object_Rock")) // 벽 닿이면 삭제
         {
-            isMoving = false;
             ani.SetBool("bulletDestroy", true);
+            gameObject.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+            gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
             Destroy(gameObject, waitForDest);
         }
 
         //똥에 박으면
         else if (collision.gameObject.CompareTag("Object_Poop"))
         {
-            isMoving = false;
             ani.SetBool("bulletDestroy", true);
             Destroy(gameObject, waitForDest);
             collision.gameObject.GetComponent<Poop>().GetDamage();
@@ -59,20 +34,9 @@ public class EnemyBullet : MonoBehaviour
         //불에 박으면
         else if (collision.gameObject.CompareTag("Object_Fire"))
         {
-            isMoving = false;
             ani.SetBool("bulletDestroy", true);
             Destroy(gameObject, waitForDest);
             collision.gameObject.GetComponent<FirePlace>().GetDamage();
-        }
-    }
-
-    // 초기에 설정된 거리에 도착하면 Destory
-    void bulletDestroy() 
-    {
-        if (Vector3.Distance(gameObject.transform.position , bulletDesti) <= 0.5f) 
-        {
-            ani.SetBool("bulletDestroy", true);
-            Destroy(gameObject, waitForDest);
         }
     }
 }
