b5f28454890fcd6e85bb158a663c1e4e2a5f08b5
Dayoeyo <dmschd0817@naver.com>
2023-11-23 17:30:47 +0900

    Soy milk

--- Assets/Item/Passive/04_Soy Milk_OK/SoyMilk.cs
+++ Assets/Item/Passive/04_Soy Milk_OK/SoyMilk.cs
@@ -11,13 +11,13 @@ public class SoyMilk : ItemInfo
         SetItemString("두유",
                       "쉬 마려워..",
                       "습득 시 공격력 * 0.2" 
-                    + "\n공격속도 * 5.5");
+                    + "\n공격속도 * 3.5");
     }
 
     public override void UseItem()
     {
         PlayerManager.instance.playerDamage *= 0.2f;
-        PlayerManager.instance.playerShotDelay /= 5.5f;
+        PlayerManager.instance.playerShotDelay /= 3.5f;
         PlayerManager.instance.playerTearSize *= 0.45f;
         PlayerManager.instance.ChgTearSize();
         base.UseItem();
