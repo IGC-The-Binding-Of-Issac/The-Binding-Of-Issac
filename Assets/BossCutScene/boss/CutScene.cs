03f4fb5501e5dd27d9270e9824889f3e05e69663
binanuku <binanuku@gmail.com>
2023-11-29 17:28:24 +0900

    컷씬 보스 이름 스프라이트 수정

--- Assets/BossCutScene/boss/CutScene.cs
+++ Assets/BossCutScene/boss/CutScene.cs
@@ -5,18 +5,46 @@ using UnityEngine.UI;
 using DG.Tweening;
 public class CutScene : MonoBehaviour
 {
-    public Image isaacSpot;
-    public Image bossSpot;
-    public Image isaacFont;
-    public Image bossFont;
-    public Image vs;
-    public Image isaac;
+    [SerializeField] Image isaacSpot;
+    [SerializeField] Image bossSpot;
+    [SerializeField] Image isaacFont;
+    [SerializeField] Image bossFont;
+    [SerializeField] Image vs;
+    [SerializeField] Image isaac;
+    [SerializeField] Image bossImage;
+    [SerializeField] Sprite bossChgSprite;
+    [SerializeField] Sprite Gurdy;
+    [SerializeField] Sprite GurdyFont;
+    [SerializeField] Sprite Larry;
+    [SerializeField] Sprite LarryFont;
+    [SerializeField] Sprite Gemini;
+    [SerializeField] Sprite GeminiFont;
+    [SerializeField] Sprite Monstro;
+    [SerializeField] Sprite MonstroFont;
     // Start is called before the first frame update
     void Start()
     {
         RectTransform rectTransform = GetComponent<RectTransform>();
         if (gameObject.activeSelf)
         {
+            if(GameManager.instance.stageLevel == 1)
+            {
+                bossImage.sprite = Gemini;
+                bossFont.sprite = GeminiFont;
+            }
+            else if(GameManager.instance.stageLevel == 2)
+            {
+                bossImage.sprite = Monstro;
+                bossFont.sprite = MonstroFont;
+            }else if (GameManager.instance.stageLevel == 3)
+            {
+                bossImage.sprite = Larry;
+                bossFont.sprite = LarryFont;
+            }else if (GameManager.instance.stageLevel == 4)
+            {
+                bossImage.sprite = Gurdy;
+                bossFont.sprite = GurdyFont;
+            }
             isaacSpot.rectTransform.DOLocalMoveX(-250, 1f);
             isaac.rectTransform.DOShakePosition(4f, new Vector3(7f, 0, 0), 60);
             bossSpot.rectTransform.DOLocalMoveX(190, 1.2f);
